41806c50a45230232f99af05a353eb7917ee032e
tobie998 <39395710+tobie998@users.noreply.github.com>
2020-06-24 00:26:46 +0700

    Add anothers file

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ElectronicWEB
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+             routes.MapRoute(
+                name: "Cart",
+                 url: "gio-hang",
+                defaults: new { controller = "Cart", action = "Index", id = UrlParameter.Optional }
+                
+            );
+            routes.MapRoute(
+            name: "Add-Cart",
+            url: "them-gio-hang",
+            defaults: new { controller = "Cart", action = "AddItem", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "San Pham",
+                url: "detail",
+                defaults: new { controller = "SanPham", action = "Details", id = UrlParameter.Optional}
+
+          );
+        }
+    }
+}
